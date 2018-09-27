fbe8b1dc3cae7d65e1a5961a2a8d0456e7e0aa37
Kirill Gorbikov <kirillhor@gmail.com>
2018-09-27 22:28:45 +0300

    It works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let numZero = 0;
+  let n = 1;
+  while ((number / (Math.pow(5, n)))^0 > 0) {
+    numZero += (number / (Math.pow(5, n)))^0;
+    n += 1;
+  }
+
+  return numZero;
 }
